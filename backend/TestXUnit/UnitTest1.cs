94bea7a2caf5e67e44421369b6036bf62c103f9d
DamianJaszewski <damianjaszewski@gmail.com>
2023-10-05 15:21:01 +0200

    add demo test for DbContext

--- backend/TestXUnit/UnitTest1.cs
+++ backend/TestXUnit/UnitTest1.cs
@@ -1,57 +1,67 @@
 using GotIt_back;
 using GotIt_back.Models;
 using GotIt_back.Services;
-using GotItTest;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Moq;
 
 namespace TestXUnit
 {
     public class UnitTest1
     {
-        //private readonly CardService _cardService;
-
-        //public UnitTest1()
-        //{
-        //    var serviceProvider = TestStartup.CreateServiceProvider();
-
-        //    _cardService = serviceProvider.GetRequiredService<CardService>();
-        //}
-
-        //private readonly CardService _cardService;
-
-        //public UnitTest1(CardService cardService)
-        //{
-        //    _cardService = cardService;
-        //}
 
         [Theory]
-        [InlineData(4,3,7)]
+        [InlineData(4, 3, 7)]
         public void UpdateCardRepeats(double x, double y, double expected)
         {
-            //Arrange
-            //Card expected = new Card();
-            //var expected1 = 1;
-
-            //Act
-            var card = new Card();
-            var repeat = new Repeat();
-            //_cardService.UpdateCardRepeats(card, repeat);
-
-            var myDataMock = new Mock<CardService>();
-
-            var actual = card;
-            var actual1 = 1.0;
+            var actual = 1.0;
 
             AddMethod(x, y);
             void AddMethod(double x, double y)
             {
-                actual1 = x + y;          
+                actual = x + y;
             }
 
             //Assert
-            Assert.Equal(expected, actual1);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetAll_DemoTest()
+        {
+            // Arrange
+            var _contextOptions = new DbContextOptionsBuilder<DataContext>()
+               .UseInMemoryDatabase("BloggingControllerTest")
+               .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+               .Options;
+
+            using var context = new DataContext(_contextOptions);
+
+            var card = 
+            new Card
+            {
+                Answer = "",
+                Category = null,
+                CategoryId = 1,
+                CreateDate = DateTime.UtcNow,
+                Id = 0,
+                Question = "",
+                Remembered = false,
+                Repeats = new List<Repeat>()
+            };
+
+            // Act
+            context.Add(card);
+            context.SaveChanges();
+
+            var weatherForecasts = context.Cards.ToList();
+
+            // Assert
+            Assert.Equal(1, weatherForecasts.Count());
+
+            //Clean up
+            context.Database.EnsureDeleted();
         }
     }
 }
